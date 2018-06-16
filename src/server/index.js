8d94e2dc0812916be82c33b9a05118f7a36fb1d6
Petr Patek <petrpatekml@gmail.com>
2018-06-16 22:03:45 +0200

    started refactoring

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,29 @@
-const express = require('express');
 
-const app = express();
+const { ApolloServer } = require('apollo-server');
 
-app.use(express.static('dist'));
+const books = require('./dummy-data/book.js');
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
 
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
 
-app.listen(8080, () => console.log('Listening on port 8080!'));
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
