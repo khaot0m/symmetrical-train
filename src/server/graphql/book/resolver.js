e0de19808df32032be4467d69fc6e53580942756
Petr Patek <petrpatekml@gmail.com>
2018-06-17 16:52:03 +0200

    added createBook mutation

--- src/server/graphql/book/resolver.js
+++ src/server/graphql/book/resolver.js
@@ -3,5 +3,12 @@ const books = require('../../dummy-data/book.js');
 exports.resolver = {
   Query: {
     books: () => books,
-  }
+  },
+    Mutation: {
+        createBook: (parent, args, context, info) => {
+            books.push(args);
+            return args;
+        }
+    }
 };
+
--- src/server/index.js
+++ src/server/index.js
@@ -20,7 +20,7 @@ const { schema, resolver } = glue('src/server/graphql');
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
 // responsible for fetching the data for those types.
-const server = new ApolloServer({ typeDefs:  schema, resolvers: resolver });
+const server = new ApolloServer({typeDefs: schema, resolvers: resolver});
 
 // This `listen` method launches a web-server.  Existing apps
 // can utilize middleware options, which we'll discuss later.
