f5363db7471f8762bbdd1673b442d3de56355856
Petr Patek <petrpatekml@gmail.com>
2018-06-24 11:13:57 +0200

    reformated using new editorconfig

--- src/server/graphql/book/resolver.js
+++ src/server/graphql/book/resolver.js
@@ -5,23 +5,23 @@ const books = require('../../dummy-data/book.js');
 
 const BOOK_ADDED = 'BOOK_ADDED';
 exports.resolver = {
-    Query: {
-        books: () => books,
+  Query: {
+    books: () => books,
+  },
+  Mutation: {
+    createBook: (parent, args, context, info) => {
+      books.push(args);
+      pubSub.publish(BOOK_ADDED, {bookCreated: args});
+      return args;
+    }
+  },
+  Subscription: {
+    bookCreated: {
+      subscribe: () => {
+        pubSub.asyncIterator([BOOK_ADDED]);
+        console.log('SUBSCRIBING');
+      }
     },
-    Mutation: {
-        createBook: (parent, args, context, info) => {
-            books.push(args);
-            pubSub.publish(BOOK_ADDED, {bookCreated: args});
-            return args;
-        }
-    },
-    Subscription: {
-        bookCreated: {
-            subscribe: () => {
-                pubSub.asyncIterator([BOOK_ADDED]);
-                console.log('SUBSCRIBING');
-            }
-        },
-    },
+  },
 };
 
