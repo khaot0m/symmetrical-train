639d144f486051df506e51ae8db912ec1671f97c
Petr Patek <petrpatekml@gmail.com>
2018-06-24 15:12:38 +0200

    created basic subscription prototype.

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@
 ### GraphQl
 better file structure: https://hackernoon.com/graphql-schemas-easier-better-structure-31677a640d18
 subscriptions: https://blog.graph.cool/how-to-build-a-real-time-chat-with-graphql-subscriptions-and-apollo-d4004369b0d4
+
+https://www.apollographql.com/docs/react/advanced/subscriptions.html
--- src/client/App.js
+++ src/client/App.js
@@ -1,16 +1,69 @@
 import React, { Component } from 'react';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+
+import client from './graphql-client';
 import './app.css';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+const BOOK_QUERY = gql`
+  {
+    books{
+      title
+      author
+    }
   }
+`;
 
+const BOOK_SUBSCRIPTION = gql`
+  subscription bookCreated{
+    bookCreated{
+      title
+      author
+    }
+  }
+`;
+class BooksPage extends Component {
+  componentDidMount() {
+    this.props.subscribeToNewComments();
+  }
   render() {
+    console.log(this.props, 'PROPS');
     return (
-      <div>
-        <h1>CUZ!</h1>
-      </div>
+      this.props.data.books ? this.props.data.books.map(({ author, title }) => <div>{author}:{title} </div>) : <h4>No Books</h4>
     );
   }
 }
+const Books = () =>
+  (<Query
+    query={BOOK_QUERY}
+  >{({ subscribeToMore, ...result }) => (
+    <BooksPage
+      {...result}
+      subscribeToNewComments={() =>
+        subscribeToMore({
+          document: BOOK_SUBSCRIPTION,
+          updateQuery: (prev, { subscriptionData }) => {
+            console.log(prev, subscriptionData, 'SAS');
+            if (!subscriptionData.data) return prev;
+            const newBook = subscriptionData.data.bookCreated;
+            console.log(prev, subscriptionData, 'SAS');
+            return Object.assign({},{
+              books: prev.books.concat(subscriptionData.data.bookCreated)
+            })
+        }
+        })
+      }
+    />
+  )}
+   </Query>);
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <h2>My first Apollo app 🚀</h2>
+      <Books />
+    </div>
+  </ApolloProvider>
+);
+export default App;
