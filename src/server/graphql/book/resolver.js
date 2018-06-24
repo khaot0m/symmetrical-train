e20f0ae3951c4469827943c7909d6bef0d43aa43
Petr Patek <petrpatekml@gmail.com>
2018-06-24 11:56:17 +0200

    make basic subscription work

--- src/server/graphql/book/resolver.js
+++ src/server/graphql/book/resolver.js
@@ -1,4 +1,4 @@
-const {PubSub} = require('apollo-server');
+const { PubSub } = require('apollo-server');
 
 const pubSub = new PubSub();
 const books = require('../../dummy-data/book.js');
@@ -11,15 +11,15 @@ exports.resolver = {
   Mutation: {
     createBook: (parent, args, context, info) => {
       books.push(args);
-      pubSub.publish(BOOK_ADDED, {bookCreated: args});
+      pubSub.publish(BOOK_ADDED, { bookCreated: args });
       return args;
     }
   },
   Subscription: {
     bookCreated: {
       subscribe: () => {
-        pubSub.asyncIterator([BOOK_ADDED]);
         console.log('SUBSCRIBING');
+        return pubSub.asyncIterator([BOOK_ADDED]);
       }
     },
   },
